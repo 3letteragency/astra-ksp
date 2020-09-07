41e34b57ed4adce1ba10c512a4d98974ebea057f
Tyler Weldon <tylerweldon94@gmail.com>
2020-09-07 02:01:28 -0400

    Automate game archive/upload, mv some files

--- packer/build.sh
+++ packer/build.sh
@@ -3,6 +3,8 @@ set -euxo pipefail
 
 source ../project.env
 
+export GAME_ARCHIVE_URL=$(s3cmd -c $ASTRA_S3_CFG signurl $ASTRA_ASSETS_BUCKET/$S3_GAME_ARCHIVE_PREFIX/$KSP_TAR_FILENAME $(date -d 'today + 1 day' +%s))
+
 validate(){
     packer validate astra.json
 }
